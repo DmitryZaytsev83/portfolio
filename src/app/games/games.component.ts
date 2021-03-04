0fe741b7b3218075b62bb7034dc80881c864687e
Dmitry <dmitry.zaytsev83@gmail.com>
2021-03-04 13:58:18 +0300

    add basic game logic

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+
+interface Face {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-games',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
+  fieldXSize = 400;
+  fieldYSize = 600;
+  public countPerRound = 10;
+  public round = 1;
+  public facesToLeft: Face[] = [];
+  public facesToRight: Face[] = [];
+  public faceSize = 80;
+  public win = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.init();
+  }
+
+  init(): void {
+    this.fillFaces();
+  }
+
+  fillFaces(): void {
+    this.facesToLeft = [];
+    for (let i = 0; i < this.round * this.countPerRound + 1; i++) {
+      this.facesToLeft.push({
+        x: Math.floor(Math.random() * (this.fieldXSize - this.faceSize)),
+        y: Math.floor(Math.random() * (this.fieldYSize - this.faceSize))
+      });
+    }
+    this.facesToRight = this.facesToLeft.filter((e, i) => i !== this.facesToLeft.length - 1);
+  }
+
+  checkRound(target: EventTarget | null): void {
+    if ((target as HTMLElement).classList.contains('last-child')) {
+      this.round++;
+    } else {
+      this.round = 1;
+    }
+    this.fillFaces();
+  }
 }
